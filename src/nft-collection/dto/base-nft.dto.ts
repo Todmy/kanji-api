9a9fe76c4115ae9598264e37e80e150e56b15258
todmy <tolok.d@gmail.com>
2022-10-12 15:25:28 +0300

    fix: updated base-nft.dto with class-validator

--- src/nft-collection/dto/base-nft.dto.ts
+++ src/nft-collection/dto/base-nft.dto.ts
@@ -1,8 +1,23 @@
+import { Transform } from 'class-transformer';
+import { IsString, IsNotEmpty, IsDefined } from 'class-validator';
+import { pipe, trim, toLowerCase } from 'src/common/helper/cast.helper';
+
 export class BaseNftDto {
+  @Transform(({ value }) => pipe(trim, toLowerCase)(value))
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
   picture: string;
+
+  @Transform(({ value }) => pipe(trim, toLowerCase)(value))
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
   source: string;
+
+  @Transform(({ value }) => pipe(trim, toLowerCase)(value))
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
   price: string;
-  createdAt: Date;
-  deletedAt: Date;
-  modifiedAt: Date;
 }
